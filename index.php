57de6fc9012336cf5c4f140bd6eae3af6aad1d64
Flamest <diegogr_tm@hotmail.com>
2016-09-05 16:02:45 -0300

    Revert "Update index.php"
    This reverts commit 6125a32469500fdff34e0b2d37b9c32b78144c45.

--- index.php
+++ index.php
@@ -1,8 +1,86 @@
-<html>
- <head>
-  <title>Prueba de PHP</title>
- </head>
- <body>
- <p>Hola mundo</p>
- </body>
-</html>
+
+<HTML>  
+<HEAD>  
+<TITLE>Un Titulo para el Browser de turno</TITLE>  
+</HEAD>  
+
+<BODY>  
+<!-- Aqui va todo lo chachi -->  
+<H1>Otro t&iacute;tulo, esta vez m&aacute;s largo.</H1>  
+
+
+<P><IMG SRC="./felix.gif" ALIGN="MIDDLE" ALT="EL Gato Felix">Hoola.
+
+
+<P>Esto es un p&aacute;rrafo con informaci&oacute;n  
+super importante. N&oacute;tese que las lineas salen pegadas aun dejando  
+ 
+espacios, saltos de l&iacute;nea, etc.<BR> &#161 Si pongo esto  
+s&iacute; <STRONG>cambia</STRONG> de l&iacute;nea!  
+
+<P>Otro p&aacuterrafo, esto ya es un poco rollo.  
+
+<H3> Pongamos un subt&iacute;tulo </H3>
+
+
+<P>Por cierto,  &#191 que pas&oacute; con las <A HREF="#pepe">anclas</A>?
+
+<HR>
+
+<UL>  
+    <LI> Esto es una lista no ordenada.  
+    <LI> Las listas quedan mejor s&iacute; tienen varios elementos.  
+    <LI> con tres es suficiente.
+</UL>
+
+<P>Me voy al <A HREF="http://www.iac.es/home.html">IAC</A>.
+
+<P>
+
+<P>Vamos a crear un <EM>ancla</EM>, o lo que es lo mismo, un <A NAME="pepe">anchor.</A>
+
+<P>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+bla,<BR>
+y m&aacute;s bla.
+
+
+</BODY>  
+</HTML>  
